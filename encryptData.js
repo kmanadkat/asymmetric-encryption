fc511e989bff21f7036f6134eb68878ff8355988
Krupesh Anadkat <kmanadkat@gmail.com>
2021-06-13 00:40:30 +0530

    Completed encryptAndSave function

---
+++ encryptData.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+const path = require('path')
+const crypto = require('crypto')
+
+const encryptAndSave = () => {
+  // 1. Bring Public Key From data/keys.json
+  const keysJsonStr = fs.readFileSync(path.join(__dirname, './data/keys.json')).toString()
+  const { publicKey } = JSON.parse(keysJsonStr)
+  const publicKeyObj = crypto.createPublicKey({ key: publicKey, type: 'pkcs1', format: 'pem' })
+
+  // 2. Bring Raw Data From data/rawData.json
+  const rawDataStr = fs.readFileSync(path.join(__dirname, './data/rawData.json')).toString()
+  const rawDataObj = JSON.parse(rawDataStr)[0]
+
+  // 3. Encrypt Raw Data
+  const encryptedData = crypto.publicEncrypt(
+    {
+      key: publicKeyObj,
+      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+      oaepHash: 'sha256',
+    },
+    Buffer.from(JSON.stringify(rawDataObj))
+  )
+  const encryptedDataStr = encryptedData.toString('base64')
+
+  // 4. Form Data To Save
+  const encryptedDataArr = [
+    {
+      id: new Date().getTime(),
+      data: encryptedDataStr,
+    },
+  ]
+
+  // 5. Save Data to data/encryptedData.json
+  const encryptedDataArrStr = JSON.stringify(encryptedDataArr)
+  fs.writeFileSync(path.join(__dirname, './data/encryptedData.json'), encryptedDataArrStr)
+}
+
+module.exports = encryptAndSave
